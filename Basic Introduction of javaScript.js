ceb88f87a643a557321ccfda1306e908a6e7dd98
Abhay kumar <abhayku2002@gmail.com>
2022-10-04 15:06:17 +0530

    Create Basic Introduction of javaScript.js
    created file for JavaScript

---
+++ Basic Introduction of javaScript.js
@@ -0,0 +1,83 @@
+console.log("JavaScript")
+
+
+//First Understand "How to intialize Comments"
+//1. JavaScript is fun. Single line comment
+/*
+2. comment are shown here. Multiple line comment
+*/
+
+// Different types of variable
+var name= "John";
+const PI= 3.14;
+let value=" david";
+
+//Understand console.log
+var a;
+var b=5;
+console.log(a);
+a=10;
+console.log(a);
+
+//intializing variables with respect to assignment operator:
+var a = 5;//here " =  is assignment operator" 
+var b = 10;
+console.log(a);
+console.log(b);
+
+// Variable is Case Sensitivity:
+var carBrand;
+var carbrand = "Ford" ;// carBrand and carbrand are both different variables.
+console.log(carbrand);
+
+//Adding, Multiplication and division of Two Values:
+var sum =10 + 20;
+console.log(sum);
+var multi, divide;
+multi = 3 * 3;
+divide =  10/2;
+console.log(multi);
+console.log(divide);
+
+// Increment and Decrement Operator:
+
+var increment= 50;
+increment++;
+console.log(increment);
+
+var decrement = 40;
+decrement--;
+console.log(decrement);
+
+// Intialize a Decimal Numbers:
+var decimal= 0.234;
+console.log(decimal);
+
+// reminder values:
+
+var reminder;
+reminder = 11%3;
+console.log(reminder);
+ 
+//Augment MAth  Operator:
+var m1=22;
+var m2=14;
+m1 += 20;
+console.log(m1);
+m2 -= 2;
+console.log(m2);
+m1 *=5;
+m2 /= 2;
+console.log(m1,m2);
+
+//Concatenate a String:
+var str = "My name is" + " John";
+console.log(str);
+
+
+
+//bracket notation:
+var first ="kevin";
+var name_value= first[2];
+console.log(name_value);
+
